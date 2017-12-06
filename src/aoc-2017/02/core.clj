375f90431635e4fd9844174904a3648813209d0a
Tyler Butters <dapegral@gmail.com>
2017-12-05 21:38:14 -0500

    Add day 2 part 2; Evenly divisible numbers in sets

--- src/aoc-2017/02/core.clj
+++ src/aoc-2017/02/core.clj
@@ -2,22 +2,33 @@
   (:require [clojure.test :refer [are is with-test]]
             [clojure.string :as str]))
 
-(defmacro applier
-  [f]
-  `(def ~(symbol (str (name f) "s"))
-     (fn [xs#] (apply ~f xs#))))
-
-(applier max)
-(applier min)
-(applier -)
+(def maxs (partial apply max))
+(def mins (partial apply min))
+(def -s (partial apply -))
 
 (defn diff-max-min
   [nums]
   (-s ((juxt maxs mins) nums)))
 
+(defn divisor-or-zero
+  [n div]
+  (let [ans (/ n div)]
+    (if (integer? ans) ans 0)))
+
+(defn only-even-divisors
+  [nums]
+  (loop [[x & xs] (sort > nums)]
+    (if-let [res (some #(when (not (zero? %)) %)
+                       (map (partial divisor-or-zero x) xs))]
+      res
+      (if (= (count xs) 0) 0 (recur xs)))))
+
 (defn checksum
-  [data]
-  (reduce #(+ %1 (diff-max-min %2)) 0 data))
+  [transformer data]
+  (reduce + 0 (map transformer data)))
+
+(def checksum1 (partial checksum diff-max-min))
+(def checksum2 (partial checksum only-even-divisors))
 
 (defn digit-array
   [s]
@@ -35,19 +46,25 @@
     [7 5 3] 4
     [2 4 6 8] 6)
 
-  (is (= (checksum (parse-input example)) 18)))
+  (is (= (checksum1 (parse-input example)) 18))
+
+  (are [in out] (= (only-even-divisors in) out)
+    [5 9 2 8] 4
+    [9 4 7 3] 3
+    [3 8 6 5] 2))
 
 
 (def day "02")
 (def filename (str "src/aoc-2017/" day "/input"))
 (def challenge-input (slurp filename))
 
-(def solve-01 #(checksum (parse-input challenge-input)))
+(def solve-01 #(checksum1 (parse-input challenge-input)))
+(def solve-02 #(checksum2 (parse-input challenge-input)))
 
 (defn print-ans [day part solver] (println (str day "." part) "answer:" (solver)))
 (defn run [solvers] (dorun (map-indexed #(print-ans day (inc %1) %2) solvers)))
 
 (defn -main
   [& args]
-  (run [solve-01]))
+  (run [solve-01 solve-02]))
 
