d93334a85c827ac9ae73bc76c13339647b2622f7
Tyler Butters <dapegral@gmail.com>
2017-12-08 02:12:36 -0500

    Day 4.2: Unique Anagrams
    <3 Clojure

--- src/aoc-2017/04/core.clj
+++ src/aoc-2017/04/core.clj
@@ -3,14 +3,26 @@
             [clojure.test :refer [are is with-test]]
             [clojure.string :as str]))
 
+(defn split-spaces [s] (str/split s #"\s+"))
+(defn count-trues [xs] (count (filter identity xs)))
+
 ; distinct? checks if every element of a collection is unique. Easy!
-(defn valid-passphrase? [p] (apply distinct? (str/split p #"\s+")))
+(defn valid-passphrase? [p] (apply distinct? (split-spaces p)))
 
 (def day "04")
 (defn parse-input [day]
   (->> (aoc/input-filename day) (slurp) (str/split-lines) (remove str/blank?)))
 (def input (parse-input day))
 
-(defn part1 [] (->> (map valid? input) (filter identity) (count)))
+(defn part1 []
+  (->> (map valid-passphrase? input)
+       (count-trues)))
 ; => 455
+
+(defn part2 []
+  (->> (map split-spaces input) ; Ready input lines
+       (map (partial map sort)) ; For each word in each line, sort their characters
+       (map (partial apply distinct?)) ; For each line, check if the sorted character sequences are unique
+       (count-trues)))
+; => 186
 
