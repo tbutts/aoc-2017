cd0db4137fa46549f803a609b1e323891437e141
Tyler Butters <dapegral@gmail.com>
2017-12-10 21:45:57 -0500

    Day 06.1: "Rebucketing memory"
    This commit dedicated to the New York State Ballet!

---
+++ src/aoc-2017/06/core.clj
@@ -0,0 +1,55 @@
+(ns aoc-2017.06.core
+  (:require [aoc-2017.common :as aoc]
+            [clojure.test :refer [are is with-test]]
+            [clojure.string :as str]))
+
+(defn max-val
+  "max value of a [idx val] pairs, tie goes to first"
+  [x y]
+  (if (>= (second x) (second y)) x y))
+
+(defn max-bank
+  "[idx, val] of largest seq value"
+  [banks]
+  (reduce max-val (map-indexed vector banks)))
+
+(defn next-idx
+  "increment cur index, looping back to 0 if at the seq length, len"
+  [cur len]
+  (mod (inc cur) len))
+
+(defn redistribute
+  "increment the next bank until all blocks have been reallocated"
+  [banks idx iter times]
+  (if (>= iter times)
+    banks
+    (redistribute (assoc banks idx (inc (nth banks idx)))
+                  (next-idx idx (count banks))
+                  (inc iter)
+                  times)))
+
+(defn realloc-banks
+  "Runs the redistribute fn until a repeat distribution of banks is
+  found. Returns the number of redistribution steps until a cycle was
+  found."
+  [init]
+  (loop [seen #{init}
+         banks init]
+    (let [[idx rtimes] (max-bank banks)
+          next-banks (redistribute (assoc banks idx 0)
+                                   (next-idx idx (count banks))
+                                   0 rtimes)]
+      (if (contains? seen next-banks)
+        (count seen)
+        (recur (conj seen next-banks) next-banks)))))
+
+(with-test
+  (def scenario1 [0 2 7 0])
+
+  (is (= (realloc-banks scenario1) 5)))
+
+(def day "06")
+(def input
+  (->> (aoc/input-filename day) slurp (#(str/split % #"\s+")) (mapv #(Integer/parseInt %))))
+(defn part1 [] (realloc-banks input))
+; => 4074
