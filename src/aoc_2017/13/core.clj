5354750587b587208000538d6df1e59300da1818
Tyler Butters <dapegral@gmail.com>
2017-12-18 15:20:38 -0500

    Day 13.1: "Really Bad Firewall Rules" 🔥 🛑
    This commit dedicated to Emacs (insert-char) command!

---
+++ src/aoc_2017/13/core.clj
@@ -0,0 +1,38 @@
+(ns aoc-2017.13.core
+  (:require [clojure.test :refer [are is with-test]]
+            [clojure.string :as str])
+  (:use [aoc-2017.common]))
+
+(defn parse-line [line] (->> (str/split line #": ") (map parse-int)))
+(defn str->firewall [input]
+  (->> input str/split-lines (mapcat parse-line) (apply array-map)))
+
+(defn tri-wave
+  [period time]
+  (- period (abs (- (mod time (* 2 (max period 1))) period))))
+
+(def caught? (comp zero? tri-wave))
+
+(defn trip-severity [alerts]
+  (reduce + (map (partial apply *) alerts)))
+
+(defn take-trip
+  "Finds all the alerts triggered while the packet passes through the firewall."
+  [fw]
+  (reduce (fn [alerts [r d :as scanner]]
+               (if (caught? (dec d) r) (conj alerts scanner) alerts))
+          [] fw))
+
+
+(defn part1 []
+  (->> (input-for-day "13") parse take-trip trip-severity))
+;; => 1612
+
+(with-test
+  (def example
+"0: 3
+1: 2
+4: 4
+6: 4")
+
+  (is (= (->> (parse example) take-trip trip-severity) 24)))
