7436a5869d2947e34782338b7629eaf916acd713
Tyler Butters <dapegral@gmail.com>
2017-12-21 08:49:03 -0500

    Day 17.2: "Counting to 1!" 📉
    This commit dedicated to counting, particularly from 5 down to 1.
    
    "COUNTING!"

--- src/aoc_2017/17/core.clj
+++ src/aoc_2017/17/core.clj
@@ -10,22 +10,39 @@
   of the spinlock algorithm. Each step of the spinlock, the `pos`ition pointer
   moves forward `step` number of times (looping if necessary). Then, the next
   `val` is inserted after the stopping position into `buf`."
-  ([step] (spinlock step [0] 0 1))
-  ([step buf pos val]
-   (let [p (inc (mod (+ pos step) (count buf)))
-         b (insert buf p val)]
-     (cons [p b] (lazy-seq (spinlock step b p (inc val)))))))
+  ([step] (spinlock step [0] 0))
+  ([step buf pos]
+   (let [buf-len (count buf)
+         p (inc (mod (+ pos step) buf-len))
+         b (insert buf p buf-len)]
+     (cons [p b] (lazy-seq (spinlock step b p))))))
 
 (defn festive-spinlock [step]
   (let [[pos2017 buf] (->> (spinlock step) (drop 2016) first)]
     (buf (inc pos2017))))
 
+(defn short-spinlock
+  "For part 2, the spinlock algo will never touch where '0' is (first position),
+  so the solution only needs to track what's in index 1 - `tracked` -
+  for 5m runs (see [0]).  The buffer size is always equal to the
+  iteration count, `iter`."
+  ([step] (short-spinlock step 0 0 1))
+  ([step tracked pos iter]
+   (let [p (inc (mod (+ pos step) iter))
+         one (if (= p 1) iter tracked)]
+     (cons one (lazy-seq (short-spinlock step one p (inc iter)))))))
 
 (def input 377)
-(defn part1 [] (festive-spinlock 377)) ;; => 596
+(defn part1 [] (festive-spinlock input)) ;; => 596
+(defn part2 [] (->> (short-spinlock input) (drop (dec 5e7)) first)) ;; => 39051595
 
 (with-test
   (def example)
 
-  (is (= (festive-spinlock 3) 638)))
+  (is (= (festive-spinlock 3) 638))
+
+  ;; [0]
+  (is (every? zero?
+              (map (fn [[_ v]] (.indexOf v 0))
+                   (take 1000 (spinlock input))))))
 
