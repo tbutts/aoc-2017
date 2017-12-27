fbbdf2706bf93ec04e7cc3d8278e327a9efa5840
Tyler Butters <dapegral@gmail.com>
2017-12-26 23:09:19 -0500

    Day 22.2: "Pandemic 2" 💉
    This commit dedicated to "Captain Sonar"!
    https://www.asmodee.us/en/games/captain-sonar/

--- src/aoc_2017/22/core.clj
+++ src/aoc_2017/22/core.clj
@@ -11,9 +11,9 @@
    :w [0 -1]})
 
 (def turns-map
-  "Maps cardinal dirs to their left-or-right positions"
-  (into {} (apply map #(vector %1 {:left %2 :right %3})
-                  ((juxt identity #(rotate-r 1 %) #(rotate 1 %)) (keys dirs-map)))))
+  "Maps cardinal dirs to their rotated positions"
+  (into {} (apply map #(vector %1 {:left %2 :right %3 :forward %1 :reverse %4})
+                  ((juxt identity #(rotate-r 1 %) #(rotate 1 %) #(rotate 2 %)) (keys dirs-map)))))
 
 (defn move-toward [dir [y x]]
   (let [[dy dx] (dirs-map dir)]
@@ -32,13 +32,36 @@
 
 (defn infected? [ch] (= ch \#))
 
+(def genesis-fns "First generation (Pandemic 1)"
+  {:was-infected? (complement infected?)
+   :turn-with     (fn [node-state] (if (infected? node-state) :right :left))
+   :infect-with   (fn [prev-node-state grid pos]
+                    (if (infected? prev-node-state)
+                      (dissoc grid pos)
+                      (assoc grid pos \#)))})
+
+(def evolved-fns "Second generation (Pandemic 2)"
+  {:was-infected? (fn [prev-node-state] (= \W prev-node-state))
+   :turn-with     (fn [node-state]
+                    (case node-state
+                      \W :forward
+                      \# :right
+                      \F :reverse
+                      nil :left))
+   :infect-with   (fn [prev-node-state grid pos]
+                    (case prev-node-state
+                      nil (assoc grid pos \W)
+                      \W  (assoc grid pos \#)
+                      \#  (assoc grid pos \F)
+                      \F  (dissoc grid pos)))})
+
 ;; The 'grid' here isn't a 2d array, but a hash-map of coordinate point -> character.
 ;; This lets the grid expand arbitrarily in any direction without needing to resize
 ;; and shift arrays around. This is why init-pos needs to be supplied separately,
 ;; because the initial grid doesn't have a 'center', because uninfected nodes aren't
 ;; tracked.
 (defn bursts
-  [init-pos init-grid iterations]
+  [strain-fns init-pos init-grid iterations]
   (loop [i 0
          infections 0
          grid init-grid
@@ -46,23 +69,28 @@
                                         :dir :n}]
     (if (>= i iterations)
       infections
-      (let [was-infected?  (infected? (get grid pos))
+      (let [node-state     (get grid pos)
             new-dir        (get-in turns-map ; Turn right if node is infected, else left
-                                   [dir (if was-infected? :right :left)])
+                                   [dir ((:turn-with strain-fns) node-state)])
             new-pos        (move-toward new-dir pos)]
         (recur
          (inc i)
-         (+ infections (if-not was-infected? 1 0))
-         (if was-infected?
-           (dissoc grid pos)
-           (assoc grid pos \#))
+         (+ infections (if ((:was-infected? strain-fns) node-state) 1 0))
+         ((:infect-with strain-fns) node-state grid pos)
          (assoc carrier :pos new-pos :dir new-dir))))))
 
-(defn play-pandemic [iterations input]
-  (let [cleaned (-> input (.trim) str/split-lines)]
-    (bursts (starting-pos cleaned) (lines->sparse-grid cleaned) iterations)))
+(defn play-pandemic
+  ([iterations input] (play-pandemic genesis-fns iterations input))
+  ([strain-fns iterations input]
+   (let [cleaned (-> input (.trim) str/split-lines)]
+     (bursts strain-fns (starting-pos cleaned) (lines->sparse-grid cleaned) iterations))))
 
-(defn part1 [] (->> (input-for-day "22") (play-pandemic 10000)))
+(def play-pandemic-2 (partial play-pandemic evolved-fns))
+
+
+(defn part1 [] (->> (input-for-day "22") (play-pandemic   10000)))    ; => 5552
+(defn part2 [] (->> (input-for-day "22") (play-pandemic-2 10000000))) ; => 2511527
+;; Sluggish, execution time is 28 seconds.
 
 (with-test
   (def example "
@@ -70,5 +98,7 @@
 #..
 ...")
 
-  (is (= (->> example (play-pandemic 10000)) 5587)))
+  (is (= (->> example (play-pandemic   10000))    5587))
+  (is (= (->> example (play-pandemic-2 100))      26))
+  (is (= (->> example (play-pandemic-2 10000000)) 2511944)))
 
